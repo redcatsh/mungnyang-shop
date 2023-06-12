8ed7314aedb2a15ac036ce186865286ac2e38452
redcatsh <redminnie56@naver.com>
2023-06-12 23:40:27 +0900

    [add] firebase initialize

---
+++ src/api/firebase.js
@@ -0,0 +1,11 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
