367ac6538c39054859ba8b19444e8fd39414f8ce
redcatsh <redminnie56@naver.com>
2023-06-19 23:56:09 +0900

    [feat] main slide

---
+++ src/components/MainSlider.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css/bundle";
+
+export default function MainSlider() {
+  return (
+    <Swiper
+      modules={[Navigation, Pagination, Scrollbar]}
+      spaceBetween={50}
+      slidesPerView={1}
+      loop={true}
+      navigation
+      pagination={{ clickable: true }}
+      scrollbar={{ draggable: true }}
+      onSwiper={(swiper) => console.log(swiper)}
+      onSlideChange={() => console.log("slide change")}
+      className="h-screen"
+    >
+      <SwiperSlide>Slide 1</SwiperSlide>
+      <SwiperSlide>Slide 2</SwiperSlide>
+      <SwiperSlide>Slide 3</SwiperSlide>
+      <SwiperSlide>Slide 4</SwiperSlide>
+      ...
+    </Swiper>
+  );
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,10 @@
 import React from "react";
+import MainSlider from "../components/MainSlider";
 
 export default function Home() {
-  return <div></div>;
+  return (
+    <>
+      <MainSlider />
+    </>
+  );
 }
