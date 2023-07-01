53527bfea511d5d479224090c6414a58c7adb0f2
redcatsh <redminnie56@naver.com>
2023-07-01 16:44:38 +0900

    [add] 상세페이지 ui

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
 import { PriceReg } from "../commons/util";
-import Button from "../components/ui/Button";
+import { ScrollRestoration } from "react-router-dom";
+import "../styles/Detail.css";
 
 export default function ProductDetail() {
   const {
@@ -10,26 +11,33 @@ export default function ProductDetail() {
     },
   } = useLocation();
   return (
-    <section className="pt-[72px]">
-      <div className="flex flex-row">
-        <div>
+    <section className="pt-[100px] pb-24 max-w-[1200px] mx-auto pr-16 pl-16">
+      <ScrollRestoration />
+      <div className="flex flex-row flex-wrap">
+        <div className="basis-[100%] sm:basis-[55%]">
           <img src={image} alt={title} />
         </div>
-        <div>
-          <p>{category}</p>
-          <h5>{title}</h5>
-          <p>₩{PriceReg(price)}</p>
+        <div className="ml-10 flex-1">
+          <p className="text-gray-500 text-sm mb-1">{category}</p>
+          <h5 className="text-2xl font-medium mb-2">{title}</h5>
+          <p className="text-xl font-medium mb-5">₩{PriceReg(price)}</p>
 
-          <p>{description}</p>
+          <p className="text-lg pt-4 desc mb-5">{description}</p>
           <div>
-            <select name="size" id="size-select">
+            <select
+              name="size"
+              id="size-select"
+              className="uppercase text-lg mb-7 p-1 w-full outline-none optionBox"
+            >
               <option value="small">{option[0]}</option>
               <option value="medium">{option[1]}</option>
               {option[2] ? <option value="large">{option[2]}</option> : ""}
             </select>
           </div>
 
-          <Button text="장바구니에 추가" />
+          <button className="font-medium text-lg w-full uppercase bg-brand text-white py-3 px-4 rounded-sm hover:brightness-110">
+            장바구니에 추가
+          </button>
         </div>
       </div>
     </section>
